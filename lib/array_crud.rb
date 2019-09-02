f9c10d7dd8897b4652dab7a468b18218b074a180
Keri Deemer <kerid18@att.net>
2019-09-02 21:11:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  many_colors = ["red", "blue", "black", "yellow"]
-  many_colors << "yellow"
+  ["red", "blue", "black", "yellow"] << "yellow"
 end
 
 def add_element_to_start_of_array(array, element)
